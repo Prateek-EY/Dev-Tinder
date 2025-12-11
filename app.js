96fc880e12e41226bfd3923197680eb4306d7871
Prateek Dev <prateek-dev@pluralsight.com>
2025-12-11 15:13:34 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req,res) => {
+  res.send('Hello World');
+});
+
+app.use("/", (req,res) => {
+    res.send('Welcome to the Home Page');
+});
+
+app.use("/test", (req,res) => {
+  res.send('Test route working');
+});
+
+
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
