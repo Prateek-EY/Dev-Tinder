48b519761aee461f49bc5e5b43715c0599305701
Prateek Dev <prateek-dev@pluralsight.com>
2025-12-15 12:52:56 +0530

    Adding validations to the Schema

--- app.js
+++ app.js
@@ -34,6 +34,33 @@ app.get('/user', async (req, res) => {
     }
 });
 
+app.get('/feed', async (req,res) => {
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }
+    catch(error){
+        console.error("Error in /feed route:", error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+app.patch('/user/:id', async (req, res) => {
+    try {
+        const userId = req.params.id;
+
+        const userData = await User.findByIdAndUpdate(userId, req.body, { runValidators: true });
+        if (!userData) {
+            return res.status(404).send("User not found");
+        }
+        res.send(userData);
+    } catch (err) {
+        console.error("Error in /user/:id route:", err);
+        res.status(500).send("Internal Server Error");
+    }   
+});
+      
+
 console.log("After /signup route");
 
 connectDb().then(() => {
--- models/user.js
+++ models/user.js
@@ -1,12 +1,25 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    firstName: { type: String },
+    firstName: { type: String ,
+    required: true,
+    minlength: 4,
+    maxlength: 50
+    },
     lastName: { type: String },
-    email: { type: String, unique: true },
-    password: { type: String },
+    email: { type: String, unique: true, required: true },
+    password: { type: String , required: true},
     age: { type: Number },
-    gender: { type: String }
+    gender: { type: String,
+        validate(value){
+            if(!["male", "female", "other"].includes(value.toLowerCase())){
+                throw new   Error("Gender must be   Male, Female or Other");
+            }
+        }
+     },
+    skills : { type: [String], default: [] }
+},{
+    timestamps: true
 });
 
 const User = mongoose.model('User', userSchema);
